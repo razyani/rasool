6e64cdb43ae5f093c008c62d8f61fb7ae7c27dee
razyani <mohsenrazyani@mailfa.com>
2016-07-20 17:20:16 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,8 +228,8 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
-    disabled_channels = {}
+    sudo_users = {154853413},
+    disabled_channels = {154853413}
   }
   serialize_to_file(config, './data/config.lua')
   print ('saved config into ./data/config.lua')
